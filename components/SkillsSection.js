a0258dce53357d7f50f815176f7757cf09951936
wassim <wassimbenjdida@gmail.com>
2021-11-28 15:37:25 +0100

    added the important sections

---
+++ components/SkillsSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function SkillsSection() {
+  return (
+    <section>
+      <span className="font-semibold text-lg text-gray-500 block mb-2">
+        Skills
+      </span>
+
+      <div className="flex items-center flex-wrap">
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Javascript
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Typescript
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Golang
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Node.js
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Software Development
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          MySQL
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Postgres
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          MongoDB
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Redis
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Git
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          Docker
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          HTML
+        </span>
+        <span className="bg-gray-800 px-3 py-1 rounded-full mr-1 mb-1 inline-block">
+          CSS
+        </span>
+      </div>
+    </section>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,85 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-// import Link from 'next/link'
-// import Date from '../components/date'
+import Head from "next/head";
+import Layout from "../components/layout";
+import AboutSection from "../components/AboutSection";
+import ExperienceSection from "../components/ExperienceSection";
+import ProjectsSection from "../components/ProjectsSection";
+import SkillsSection from "../components/SkillsSection";
 
 export default function Home() {
   return (
-    <Layout home>
+    <Layout>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Rizume</title>
       </Head>
-      <span className={utilStyles.headingXl}></span>
+
+      {/* Header */}
+      <header className="py-10 px-20 border-b border-gray-800">
+        <div className="flex justify-between flex-wrap">
+          <div>
+            <span className="text-4xl font-bold text-gray-300 block">
+              Wassim Ben Jdida
+            </span>
+            <span className="text-2xl text-gray-400 block">
+              Software Developer
+            </span>
+          </div>
+
+          <div className="font-semibold">
+            <div
+              // TODO: show a "Copied" message
+              onClick={() =>
+                navigator.clipboard.writeText("wassimbenjdida@gmail.com")
+              }
+              className="flex mb-1 cursor-pointer hover:text-gray-300 transition"
+            >
+              <img
+                src="/images/at-sign.svg"
+                width="18px"
+                className="mr-1 block"
+              />
+              wassimbenjdida@gmail.com
+            </div>
+            <a href="#" className="flex mb-1 hover:text-gray-300 transition">
+              <img
+                src="/images/github.svg"
+                width="18px"
+                className="mr-1 block"
+              />
+              wassimbj
+            </a>
+            <a href="#" className="flex hover:text-gray-300 transition">
+              <img
+                src="/images/linkedin.svg"
+                width="18px"
+                className="mr-1 block"
+              />
+              wasssim-ben-jdida
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <main className="px-20 mt-20">
+        <div className="grid grid-cols-2">
+          <div className="col-span-1 pr-10">
+            {/* Bio */}
+            <AboutSection />
+            <br />
+            <br />
+            {/* Experience */}
+            <ExperienceSection />
+            <br />
+            <br />
+            {/* Porjects */}
+            <ProjectsSection />
+          </div>
+          {/* ------------------------------------ */}
+          <div className="col-span-1">
+            {/* Skills */}
+            <SkillsSection />
+          </div>
+        </div>
+      </main>
     </Layout>
-  )
+  );
 }
