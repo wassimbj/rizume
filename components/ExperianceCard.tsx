bab4837fc92e7bfa0a5e3fa359ffa87730c79e61
wassim <wassimbenjdida@gmail.com>
2021-11-30 06:48:21 +0100

    removed the experience timeline

--- components/ExperianceCard.tsx
+++ components/ExperianceCard.tsx
@@ -18,10 +18,7 @@ export default function ExperianceCard({
     <div className="mb-5 relative">
       {/* <span className="absolute -left-2 bg-white bg-opacity-10 top-3 w-0.5" style={{height: "calc(100% + 20px)"}}></span> */}
       <span className="block font-semibold text-gray-300">
-        <div className="relative">
-          {company}
-          <span className="absolute z-10 -left-3 bg-white bg-opacity-25 top-2 rounded-full w-2 h-2"></span>
-        </div>
+        {company}
         <span className="text-gray-700 mx-2">‣</span>
         <span className="text-white text-opacity-60 font-normal">{job}</span>
         <span className="text-gray-700 mx-2">‣</span>
