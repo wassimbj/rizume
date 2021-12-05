df16ae20aa2ef22b1c92bff32faeca2c3a4ae854
wassim <wassimbenjdida@gmail.com>
2021-12-05 17:11:00 +0100

    show a Copied message

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AtSignIcon from "./icons/AtSignIcon";
 import GithubIcon from "./icons/GithubIcon";
 import LinkedinIcon from "./icons/LinkedinIcon";
 import aboutData from "../meta/about.json";
 
 export default function Header() {
+  const [emailCopied, setEmailCopied] = useState(false);
+  const copyEmail = () => {
+    navigator.clipboard.writeText(`${aboutData.contact.email}`);
+    setEmailCopied(true);
+
+    setTimeout(() => {
+      setEmailCopied(false);
+    }, 500);
+  };
+
   return (
     <header>
       <div className="flex items-center justify-between flex-wrap">
@@ -17,22 +27,25 @@ export default function Header() {
           </span>
         </div>
 
-        <div className="font-semibold">
+        <div>
           <div
-            // TODO: show a "Copied" message
-            onClick={() =>
-              navigator.clipboard.writeText(`${aboutData.contact.email}`)
-            }
-            className="py-1 flex items-center cursor-pointer text-lightBlue text-opacity-40 hover:text-opacity-100 transition"
+            onClick={copyEmail}
+            className="relative py-1 cursor-pointer font-semibold flex items-center text-lightBlue text-opacity-40 hover:text-opacity-100 transition"
           >
             <span className="bg-lightBlue rounded-full mr-1 p-1">
               <AtSignIcon width="18" color="#000" />
             </span>
             {aboutData.contact.email}
+
+            {emailCopied && (
+              <span className="absolute inline-block p-1 font-normal text-xs animate-ping rounded-md bg-darkBlue text-lightBlue shadow-2xl -top-5 left-1/2">
+                Copied
+              </span>
+            )}
           </div>
           <a
             href={`https://github.com/${aboutData.contact.github}`}
-            className="py-1 flex items-center text-lightBlue text-opacity-40 hover:text-opacity-100 transition"
+            className="py-1 font-semibold flex items-center text-lightBlue text-opacity-40 hover:text-opacity-100 transition"
           >
             <span className="bg-lightBlue rounded-full mr-1 p-1">
               <GithubIcon width="18" color="#000" />
@@ -41,7 +54,7 @@ export default function Header() {
           </a>
           <a
             href={`https://linkedin.com/in/${aboutData.contact.linkedin}`}
-            className="py-1 flex items-center text-lightBlue text-opacity-40 hover:text-opacity-100 transition"
+            className="py-1 font-semibold flex items-center text-lightBlue text-opacity-40 hover:text-opacity-100 transition"
           >
             <span className="bg-lightBlue rounded-full mr-1 p-1">
               <LinkedinIcon width="18" color="#000" />
